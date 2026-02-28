6a82df0c5c9941f01cf41f306b53c27862c5d40f
torusk <shancun.kazuki@gmail.com>
2026-02-28 11:39:26 +0900

    Initial Commit

---
+++ Color Grid Pomodoro/Color_Grid_PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Grid_PomodoroApp.swift
+//  Color Grid Pomodoro
+//
+//  Created by kazuki on 2026/02/28.
+//
+
+import SwiftUI
+
+@main
+struct Color_Grid_PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Color Grid Pomodoro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Color Grid Pomodoro
+//
+//  Created by kazuki on 2026/02/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
